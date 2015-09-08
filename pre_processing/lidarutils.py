884dfb97cecee9ee3e208cae216d09fd52f39088
Mauro Assis <assismauro@hotmail.com>
2015-09-08 09:18:11 -0300

    New functions added
    1) splitCells(inputfilename,cellsize,verbose)
    
    Splits a LiDAR cloud file horizontally in tiles with cellsize (m) side.
    
    2) slices(inputfname,nslices,verbose)
    
    Clip a LiDAR cloud vertically in n slices.
    
    3) toFloor(inputfname,outputfname,floor,verbose)
    
    Genenate a new outputfilename LiDAR file with all z coords equal floor, that is, transform a LiDAR cloud in a 2 dimensions cloud.
    
    4) topBottom(command,inputfname,outputfname,percent,verbose)
    
    If command is 't', generates a new file containing a top slice of the inputfilename file. The slice will correspond to a 'percent' percent of the total height of the cloud.
    
    If command is 'b', generates a file containing a bottom slice of the original cloud.

--- pre_processing/lidarutils.py
+++ pre_processing/lidarutils.py
@@ -25,10 +25,22 @@ def saveCloud(fname,header,cloud):
 def ParseCmdLine():
     parser = argparse.ArgumentParser(description="Some useful features to deal with LiDAR files.",formatter_class=RawTextHelpFormatter)
     parser.add_argument("inputfname",help="las file to be process.")
-    parser.add_argument("-t","--task",choices=['a','d','x'], required=True, 
-        help="define task to be done.\r\n 'a' cut the file generating cellsize square clouds.\r\n 'd' display information about the file.\r\n 'x' export points to csv file.")
-    parser.add_argument("-o","--outputfname", help="output file name.", default = "")
+    parser.add_argument("-t","--task",choices=['p','b','d','x','s','t','f'], required=True, 
+        help="""
+define task to be done.
+  'd' display information about LAS file.
+  'f' create new file with z coord = floor parameter
+  't' create one slice from top, with p percent 
+  'b' create one slice from bottom, with p percent 
+  'p' split LAS file generating cellsize squared clouds.
+  's' slices file in 'ns' slices.
+  'x' export LAS points to csv file.
+        """)
+    parser.add_argument("-o","--outputfname", help="output file name.", default = None)
     parser.add_argument("-c","--cellsize", type= float, help="size of the cells that will be processed.", default = 50)
+    parser.add_argument("-f","--floor", type=float,help="floor value for toFloor function")
+    parser.add_argument("-ns","--nslices",type=int,help="number of slices",default=5)
+    parser.add_argument("-p","--percent", type=float,help="percent value for t or b function")    
     parser.add_argument("-v","--verbose", type=int, help="show processing messages.", default = True)
     args = parser.parse_args()
     return args
@@ -112,6 +124,68 @@ def splitCells(inputfname, cellsize=50, verbose=False):
         print
         print("Done in {0}s.".format(int(time.time()-start)))
 
+def slices(inputfname,nslices,verbose):
+    start = time.time()
+
+    warningMsg = []    
+
+    inFile = file.File(inputfname, mode = "r")
+    zmin = inFile.z.min()
+    zmax = inFile.z.max()
+    hn = (zmax-zmin)/nslices
+    accepted_logic = []              # List to save the true/false for each looping
+    for stepZ in range(nslices):
+        Z_valid = np.logical_and((zmin + (stepZ * hn) <= inFile.z),
+                                 (zmin + ((stepZ + 1) * hn) > inFile.z))
+        validZ = np.where(Z_valid)
+        
+        # show progress before 'continue'
+        if(verbose):
+            percent = float(stepZ+1)/nslices
+            hashes = '#' * int(round(percent * 20))
+            spaces = ' ' * (20 - len(hashes))
+            print("\r[{0}] {1:.2f}%".format(hashes + spaces, percent * 100)),
+        if(len(validZ[0]) == 0):
+            accepted_logic.append(False)
+            if(verbose):
+                warningMsg.append("Slice {0} has no points, corresponding file was not created.".format(stepZ))
+            continue
+             
+        fnameslice = "slice{0}_{1}".format(stepZ,os.path.basename(inputfname))
+        saveCloud(fnameslice,inFile.header,inFile.points[validZ])               
+    
+    if(verbose):
+        print
+        if (len(warningMsg) > 0):
+            print
+            print("Warnings:")
+            print("{0}".format("\r\n".join(str(i) for i in warningMsg)))
+        print
+        print("Done in {0}s.".format(int(time.time()-start)))    
+
+def toFloor(inputfname,outputfname="",floor=0,verbose=True):
+    inFile = file.File(inputfname, mode = "rw")
+    z=inFile.z
+    z.fill(floor);
+    inFile.z=z
+    outputfname = outputfname if not(outputfname == None) else "toFloor_"+os.path.basename(inputfname)
+    saveCloud(outputfname,inFile.header,inFile.points)
+
+def topBottom(command,inputfname,outputfname="",percent=5.0,verbose=True):
+    inFile = file.File(inputfname, mode = "rw")
+    r=(inFile.z.max() - inFile.z.min()) * percent / 100.0 + inFile.z.min()
+    if(command == 'b'):
+        Z_valid = np.logical_and(inFile.z <= r,True)
+        strFunc='bottom_'
+    else: # top
+        r=inFile.z.max()-r
+        Z_valid = np.logical_and(inFile.z >= r,True)
+        strFunc='top_'
+    validZ = np.where(Z_valid)
+    outputfname = outputfname if not(outputfname == None) else strFunc+os.path.basename(inputfname)
+    saveCloud(outputfname,inFile.header,inFile.points[validZ])
+        
+    
 def exportToCSV(inputfname,outputfname="",delimiter=";",verbose=True):
     inFile = file.File(inputfname, mode = "r")
     if(outputfname == ""):
@@ -125,19 +199,12 @@ def exportToCSV(inputfname,outputfname="",delimiter=";",verbose=True):
     
     output.write(headercsv+"\n")
     
-    for data_slice in inFile.points:
-        line=""
-        for l in data_slice:
-            for x in l:
-                line+="{0}{1}".format(x,delimiter)
-            output.write(line[:-1]+"\n")
-            '''
-            if verbose and ((n % 100) == 0):
-                percent = n/total
-                hashes = '#' * int(round(percent * 20))
-                spaces = ' ' * (20 - len(hashes))
-                print("\r[{0}] {1:.2f}%".format(hashes + spaces, percent * 100)),
-            '''
+    for data_tile in inFile.points:
+        txtline=""
+        for line in data_tile:
+            for value in line:
+                txtline+="{0}{1}".format(value,delimiter)
+            output.write(txtline[:-1]+"\n")
     output.close()
     
 def displayInfo(inputfname,verbose):
@@ -151,12 +218,19 @@ def main():
     warnings.simplefilter("error", RuntimeWarning)
     args=ParseCmdLine()
     checkParams(args)
-    if(args.task == 'a'):
+    if(args.task == 'p'):
         splitCells(args.inputfname,args.cellsize,args.verbose)
+    elif(args.task == 's'):
+        slices(args.inputfname,args.nslices,args.verbose)
     elif(args.task == 'd'):
         displayInfo(args.inputfname,args.verbose)
     elif(args.task == 'x'):
         exportToCSV(args.inputfname,args.outputfname,";",args.verbose)
+    elif(args.task == 'f'):
+        toFloor(args.inputfname,args.outputfname,args.floor)
+    elif((args.task == 'b') or (args.task == 't')):
+        topBottom(args.task,args.inputfname,args.outputfname,args.percent,args.verbose)
+        
         
 # In[ ]:
 
